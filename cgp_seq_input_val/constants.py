3172d533ac015fe38680240bc1eaf35abe3ddd94
Keiran Raine <kr2@sanger.ac.uk>
2017-05-06 15:20:54 +0100

    Major updates following addition of tests

--- cgp_seq_input_val/constants.py
+++ cgp_seq_input_val/constants.py
@@ -1,1 +1,4 @@
-MANIFEST_EXTNS = ('xls','xlsx','csv','tsv') # tuple not list as immutable
+# tuple not list as immutable
+MANIFEST_EXTNS = ('xls','xlsx','csv','tsv')
+# start of line denoting change between header and body
+HEADER_BODY_SWITCH = 'Group_ID'
---
+++ test_n_cover.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+nosetests-3.4 --with-coverage --cover-erase --cover-html --cover-package=cgp_seq_input_val
