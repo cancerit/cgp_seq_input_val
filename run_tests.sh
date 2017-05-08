7a78805dd0406ec8f7021a4240b93f9bd789e019
Keiran Raine <kr2@sanger.ac.uk>
2017-05-08 11:24:53 +0100

    Change to use an env variable to run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nosetests-3.4 --with-coverage --cover-erase --cover-html --cover-package=cgp_seq_input_val
+$NOSETESTS3 --with-coverage --cover-erase --cover-html --cover-package=cgp_seq_input_val
