93ec7df1c1ffd02cf8a16db64180fa0e45ff2f37
Keiran Raine <kr2@sanger.ac.uk>
2017-10-10 13:50:02 +0100

    More ways to keep on top of code

--- run_tests.sh
+++ run_tests.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-env/bin/nosetests --with-coverage --cover-erase --cover-html --cover-package=cgp_seq_input_val
-code=$?
-
-if [ "$code" != "0" ]; then
-    exit $code
-fi
+set -e
+env/bin/nosetests --with-coverage --cover-erase --cover-html --cover-min-percentage=50 --cover-package=cgp_seq_input_val
+set +e
 
 # these should not die:
-
-echo -e "\n###################################"
-echo      "# Running radon (code complexity) #"
-echo      "###################################"
-env/bin/radon cc -nc bin cgp_seq_input_val
 
 echo -e "\n##########################"
 echo      "# Running pylint (style) #"
 echo      "##########################"
 env/bin/pylint --output-format=colorized bin/*.py cgp_seq_input_val
 
+echo -e "\n#########################################"
+echo      "# Running radon (cyclomatic complexity) #"
+echo      "#########################################"
+env/bin/radon cc -nc bin cgp_seq_input_val
+
+echo -e "\n#########################################"
+echo      "# Running radon (maintainability index) #"
+echo      "#########################################"
+env/bin/radon mi -s -n B bin cgp_seq_input_val
 
 exit 0 # don't die based on assements of code quality
