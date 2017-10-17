e8bc0b4f2b8b618f6ee5611a167efc8b560f6099
Keiran Raine <kr2@sanger.ac.uk>
2017-10-17 20:50:39 +0100

    Add branch coverage and change linter

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
-pytest --cov-report term --cov-report html --cov=cgp_seq_input_val --cov-fail-under=50
+pytest --cov-branch --cov-report term --cov-report html --cov=cgp_seq_input_val --cov-fail-under=50
 set +e
 
 # these should not die:
 
-echo -e "\n##########################"
-echo      "# Running pep8 (style)   #"
-echo      "##########################"
-pep8 --format=pylint cgp_seq_input_val
+echo -e "\n#################################"
+echo      "# Running pycodestyle (style)   #"
+echo      "#################################"
+pycodestyle cgp_seq_input_val
 
 echo -e "\n#########################################"
 echo      "# Running radon (cyclomatic complexity) #"
