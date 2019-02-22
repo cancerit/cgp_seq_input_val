cbfd497fa9d49350d522526ca1fe3d27ee14deff
Yaobo Xu <yx2@sanger.ac.uk>
2019-02-22 11:02:28 +0000

    added tests on casava 1.8 fq files

--- tests/test_cgp_seq_input_val_fastq_read.py
+++ tests/test_cgp_seq_input_val_fastq_read.py
@@ -1,7 +1,7 @@
 import pytest
 import os, sys, tempfile
 
-from cgp_seq_input_val.fastq_read import FastqRead
+from cgp_seq_input_val.fastq_read import FastqRead, FastqFormat
 from cgp_seq_input_val.error_classes import SeqValidationError
 
 test_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'fastq_read')
@@ -12,40 +12,44 @@ def setup():
 def teardown():
     pass
 
-def test_fastq_header_no_at():
+
+@pytest.mark.parametrize('file_name', ['bad_header_1.fq', 'bad_header_2.fq'])
+def test_fastq_header_no_at(file_name):
     with pytest.raises(SeqValidationError) as e_info:
-        fqi = os.path.join(test_dir, 'bad_header_1.fq')
+        fqi = os.path.join(test_dir, file_name)
         with open(fqi, 'r') as fp:
-            fr = FastqRead(fp, 0, None)
-            fr.validate('x')
+            fr = FastqRead(fp, 0, None, None)
+    assert 'Unsupported FastQ header format' in str(e_info.value)
 
 
-def test_fastq_header_at_only():
+@pytest.mark.parametrize('file_name',
+        ['seq-shorter_1.fq', 'qual-shorter_1.fq',
+        'seq-shorter_1_casava_1_8.fq', 'qual-shorter_1_casava_1_8.fq']
+    )
+def test_fastq_seq_shorter_than_qual(file_name):
     with pytest.raises(SeqValidationError) as e_info:
-        fqi = os.path.join(test_dir, 'bad_header_2.fq')
+        fqi = os.path.join(test_dir, file_name)
         with open(fqi, 'r') as fp:
-            fr = FastqRead(fp, 0, None)
+            fr = FastqRead(fp, 0, None, None)
             fr.validate('x')
+    assert 'appears to be corrupt' in str(e_info.value)
 
 
-def test_fastq_seq_shorter_than_qual():
-    with pytest.raises(SeqValidationError) as e_info:
-        fqi = os.path.join(test_dir, 'seq-shorter_1.fq')
-        with open(fqi, 'r') as fp:
-            fr = FastqRead(fp, 0, None)
-            fr.validate('x')
-
-
-def test_fastq_qual_shorter_than_seq():
-    with pytest.raises(SeqValidationError) as e_info:
-        fqi = os.path.join(test_dir, 'qual-shorter_1.fq')
-        with open(fqi, 'r') as fp:
-            fr = FastqRead(fp, 0, None)
-            fr.validate('x')
+@pytest.mark.parametrize('file_format',
+    [
+        ('good_read_1.fq', FastqFormat.ILLUMINA),
+        ('casava_1_8_reads.fq', FastqFormat.CASAVA)
+    ])
+def test_fastq_seq_determine_format(file_format):
+    file_name, format = file_format
+    fqi = os.path.join(test_dir, file_name)
+    with open(fqi, 'r') as fp:
+        fr = FastqRead(fp, 0, None, None)
+        assert fr.format == format
 
 
 def test_fastq_string_print():
     fqi = os.path.join(test_dir, 'good_read_1.fq')
     with open(fqi, 'r') as fp:
-        fr = FastqRead(fp, 0, None)
+        fr = FastqRead(fp, 0, None, None)
         t = str(fr)
