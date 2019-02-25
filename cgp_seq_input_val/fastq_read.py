e0d68e1835cf14e064aeb740a33c2cad83683283
Yaobo Xu <yx2@sanger.ac.uk>
2019-02-25 15:27:46 +0000

    added two more FastQ classes so that format is not evaluate everytime before validate

--- cgp_seq_input_val/fastq_read.py
+++ cgp_seq_input_val/fastq_read.py
@@ -6,8 +6,8 @@ import re
 from enum import Enum
 from cgp_seq_input_val.error_classes import SeqValidationError
 
-ILLUMINA_FASTQ_HEADER_PATTERN = r'^@(\S+)/([12])$'
-CASAVA_FASTQ_HEADER_PATTERN = r'^@(\S+)\s([12])(:\w+:\w+:\w+)$'
+ILLUMINA_FASTQ_HEADER_PATTERN = re.compile(r'^@(\S+)/([12])$')
+CASAVA_FASTQ_HEADER_PATTERN = re.compile(r'^@(\S+)\s([12])(:\w+:\w+:\w+)$')
 
 
 class FastqFormat(Enum):
@@ -26,7 +26,7 @@ class FastqRead(object):
         curr_line: last line read from file
             - None = start of file
     """
-    def __init__(self, fq_fh, line_no_in, curr_line, format):
+    def __init__(self, fq_fh, line_no_in, curr_line):
         line_no = line_no_in
         seq_header = None
         seq = ''
@@ -37,16 +37,6 @@ class FastqRead(object):
             line_no += 1
         else:
             seq_header = curr_line
-
-        if format is None:
-            if re.match(ILLUMINA_FASTQ_HEADER_PATTERN, seq_header) is not None:
-                format = FastqFormat.ILLUMINA
-            elif re.match(CASAVA_FASTQ_HEADER_PATTERN, seq_header) is not None:
-                format = FastqFormat.CASAVA
-            else:
-                raise SeqValidationError("Unsupported FastQ header format: %s"
-                                         % seq_header)
-        self.format = format
 
         curr_line = fq_fh.readline().rstrip()
         line_no += 1
@@ -79,13 +69,20 @@ class FastqRead(object):
         # TODO needs to output casava 1.8 format headers
         return "%s\n%s\n%s\n%s" % (self.seq_header, self.seq, self.qual_header, self.qual)
 
+    def get_fq_format(self):
+        if ILLUMINA_FASTQ_HEADER_PATTERN.match(self.seq_header) is not None:
+            format = FastqFormat.ILLUMINA
+        elif CASAVA_FASTQ_HEADER_PATTERN.match(self.seq_header) is not None:
+            format = FastqFormat.CASAVA
+        else:
+            raise SeqValidationError("Unsupported FastQ header format: %s"
+                                        % self.seq_header)
+        return format
+
+
+class CasavaFastqRead(FastqRead):
+
     def validate(self, filename):
-        if self.format == FastqFormat.ILLUMINA:
-            return self._validate_illumina_format(filename)
-        elif self.format == FastqFormat.CASAVA:
-            return self._validate_casava_format(filename)
-
-    def _validate_illumina_format(self, filename):
         """
         Checks the record read conforms to expected conventions
 
@@ -95,32 +92,11 @@ class FastqRead(object):
         Raises:
             SeqValidationError - Generic errror with validation
         """
-        match = re.match(ILLUMINA_FASTQ_HEADER_PATTERN, self.seq_header)
+        match = CASAVA_FASTQ_HEADER_PATTERN.match(self.seq_header)
         if match is None:
-            raise SeqValidationError("Sequence record header must match pattern: %s, line %d of %s"
-                                     % (ILLUMINA_FASTQ_HEADER_PATTERN, self.file_pos[0], filename))
-        groups = match.groups()
-        self.name = groups[0]
-        self.pair_member = groups[1]
-
-        if len(self.qual) != len(self.seq):
-            raise SeqValidationError("Fastq record at line %d of %s appears to be corrupt"
-                                     % (self.file_pos[0], filename))
-
-    def _validate_casava_format(self, filename):
-        """
-        Checks the record read conforms to expected conventions
-
-        Args:
-            filename - filename read was read from, used in error messages
-
-        Raises:
-            SeqValidationError - Generic errror with validation
-        """
-        match = re.match(CASAVA_FASTQ_HEADER_PATTERN, self.seq_header)
-        if match is None:
-            raise SeqValidationError("Sequence record header must match patten: %s, line %d of %s"
-                                     % (CASAVA_FASTQ_HEADER_PATTERN, self.file_pos[0], filename))
+            raise SeqValidationError("Sequence record header must match patten: '%s', line %d of %s" % (
+                CASAVA_FASTQ_HEADER_PATTERN.pattern, self.file_pos[0], filename)
+            )
         groups = match.groups()
         self.name = groups[0] + ' ' + groups[2]
         self.pair_member = groups[1]
@@ -128,3 +104,29 @@ class FastqRead(object):
         if len(self.qual) != len(self.seq):
             raise SeqValidationError("Fastq record at line %d of %s appears to be corrupt"
                                      % (self.file_pos[0], filename))
+
+
+class IlluminaFastqRead(FastqRead):
+
+    def validate(self, filename):
+        """
+        Checks the record read conforms to expected conventions
+
+        Args:
+            filename - filename read was read from, used in error messages
+
+        Raises:
+            SeqValidationError - Generic errror with validation
+        """
+        match = ILLUMINA_FASTQ_HEADER_PATTERN.match(self.seq_header)
+        if match is None:
+            raise SeqValidationError("Sequence record header must match pattern: '%s', line %d of %s" % (
+                ILLUMINA_FASTQ_HEADER_PATTERN.pattern, self.file_pos[0], filename)
+            )
+        groups = match.groups()
+        self.name = groups[0]
+        self.pair_member = groups[1]
+
+        if len(self.qual) != len(self.seq):
+            raise SeqValidationError("Fastq record at line %d of %s appears to be corrupt"
+                                    % (self.file_pos[0], filename))
